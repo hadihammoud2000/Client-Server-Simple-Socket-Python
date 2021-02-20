346c74827c7ac21278f710d15c3f85525483c814
hadihammoud2000 <69105312+hadihammoud2000@users.noreply.github.com>
2021-02-20 15:02:01 +0200

    Update client.py
    -added input address, port, and message (commented the test input)
    -changed the position of starting time from after the .sendto() command to before it.

--- client.py
+++ client.py
@@ -3,21 +3,21 @@ import time
 import datetime
 
 
-receive_buffer_size = 1024
-"""IP = input("enter IP: ")
-port = int(input("enter Port: "))
-Message = input("enter echo message: ")
 """
-
-#list containing the RTT of the messages, used to compute statistics
-avgRTT=[]
-
-
-#IP port and echo message input
 IP = '127.0.0.1'
 port = 20001
 Echo = "This is an ECHO message"
+"""
 
+receive_buffer_size = 1024
+#IP port and echo message input
+IP = input("enter IP: ")
+port = int(input("enter Port: "))
+Echo = input("enter echo message: ")
+
+
+#list containing the RTT of the messages, used to compute statistics
+avgRTT=[]
 
 #connecting to client socket
 ClientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -25,12 +25,14 @@ ClientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 #for loop to send 5 requests
 for i in range(5):
+    
+    
+    #computing time before sending the message to server
+    startTime = time.time()
+
 
     #sending the message using method sendto()
     sending = ClientSocket.sendto(Echo.encode('utf-8'),(IP,port))
-
-    #computing time before sending the message to server
-    startTime = time.time()
 
 
     #receiving the message back from server (following echo protocol)
